fbdf7e5bf76af567d8d02c9c4f4cb8725ed8e333
Aymen Ben Hamida <baymenemir@gmail.com>
2024-09-23 02:14:21 +0100

    added the csv file creation and improved the logic to better suit the challenge

--- src/controllers/artistController.ts
+++ src/controllers/artistController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { searchArtist } from "../services/artistService";
-// import { writeCSV } from "../utils/csvWriter";
-// import randomArtists from "../data/randomArtists.json";
+import { writeCSV } from "../utils/csvWriter";
+import randomArtists from "../data/randomArtists.json";
+import Artist from "../interfaces/Artist";
 //will handle the logic of finding the artists
 export const searchArtistByName = async (req: Request, res: Response) => {
   const { name, fileName } = req.query as { name?: string; fileName?: string };
@@ -9,17 +10,27 @@ export const searchArtistByName = async (req: Request, res: Response) => {
   if (!fileName) {
     return res.status(400).json({ error: "Please provide a fileName" });
   }
+  if (!name) {
+    return res.status(400).json({ error: "Please provide an artist's name" });
+  }
   try {
+    //initializing a list of artists
+    let artistList: Artist[] = [];
     //search for artist by name
-    const result = await searchArtist(name || "");
-    return res.status(200).json({ message: "Success", data: result });
-    // if (result.length === 0 && !name) {
-    //   for (const randomArtist of randomArtists) {
-    //     const retry = await searchArtist(randomArtist);
-    //   }
-    // } else {
-    // }
+    artistList = await searchArtist(name);
+    // when the no artists are returned or no name is given get artists based on a random name
+    if (artistList.length === 0) {
+      let index = Math.floor(Math.random() * randomArtists.length);
+      const newArtistList = await searchArtist(randomArtists[index]);
+      artistList.push(...newArtistList);
+    }
+    //beginning the writing of the csv file
+    await writeCSV(fileName, artistList);
+    //return data
+    return res.status(200).json({ message: "Success", data: artistList });
   } catch (error) {
+    console.error(error);
+    //returning error
     res.status(500).json({ error: "Error fetching artist data" });
   }
 };
--- src/utils/csvWriter.ts
+++ src/utils/csvWriter.ts
@@ -1,33 +1,35 @@
 import { createObjectCsvWriter } from "csv-writer";
 import Artist from "../interfaces/Artist";
+import { createWriteStream } from "fs";
+import { format } from "fast-csv";
+import { resolve } from "path";
+import { rejects } from "assert";
 //a function that creates a csv file depending on the parametres
 export const writeCSV = async (
   fileName: string,
   data: Artist[]
 ): Promise<void> => {
-  //creation of the csv-writer main object with the file's columns and it's name
-  const csvWriter = createObjectCsvWriter({
-    path: `${fileName}.csv`,
-    header: [
-      { id: "name", title: "Name" },
-      {
-        id: "mbid",
-        title: "Mbid",
-      },
-      {
-        id: "url",
-        title: "Url",
-      },
-      {
-        id: "image_small",
-        title: "Image Small",
-      },
-      {
-        id: "image",
-        title: "Image",
-      },
-    ],
+  //setting the headers for the csv file
+  const csvStream = format({
+    headers: ["name", "mbid", "url", "image_small", "image"],
   });
-  //creating the actual file
-  await csvWriter.writeRecords(data);
+  //setting the name of teh fiel
+  const writableStream = createWriteStream(`${fileName}.csv`);
+  //when the writing is done the promise is resolved
+  writableStream.on("finish", resolve);
+  //and here the promise is rejected on error
+  writableStream.on("error", rejects);
+  // pipe the CSV stream to the writable stream (file)
+  csvStream.pipe(writableStream);
+  // Write each artist's data to the CSV
+  data.forEach((artist) => {
+    csvStream.write({
+      name: artist.name || "",
+      mbid: artist.mbid || "",
+      url: artist.url || "",
+      image_small: artist.image_small || "",
+      image: artist.image || "",
+    });
+  });
+  csvStream.end();
 };
